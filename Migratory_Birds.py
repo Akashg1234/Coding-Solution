7255b6044ad9c0dac10b7cbaf91065c4c20d3da3
Akash Goswami <46678116+Akashg1234@users.noreply.github.com>
2020-05-02 10:41:33 +0530

    Create Migratory_Birds.py

---
+++ Migratory_Birds.py
@@ -0,0 +1,13 @@
+n=int(input())
+l1=[]
+count_list=[]
+
+for i in range(n):
+  k=int(input())
+  #print(k)
+  l1.append(k)
+
+for i in range(1,6):
+  count_list.append(l1.count(i))
+  #[val, idx] = max(count_list); 
+print(l1[count_list.index(max(count_list))])
