572397b142b143737e3956106ecd12c6749fdbac
Akash Goswami <46678116+Akashg1234@users.noreply.github.com>
2022-08-15 22:44:37 +0530

    Create RomanToInteger.py

---
+++ RomanToInteger.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def romanToInt(self, s: str) -> int:
+        rule={
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000,
+        }
+        res=0
+        
+        s = s.replace('IV','IIII').replace('IX','VIIII').replace('XL','XXXX').replace('XC','LXXXX').replace('CD','CCCC').replace('CM','DCCCC')
+        
+        for i in s:
+            res+= rule[i]
+                
+        return res
