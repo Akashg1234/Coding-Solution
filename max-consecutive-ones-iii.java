ccdb8b3bd1ae92591849c4aa4ebe34f17d8eb023
Akash Goswami <46678116+Akashg1234@users.noreply.github.com>
2022-12-23 22:54:55 +0530

    Create max-consecutive-ones-iii.java

---
+++ max-consecutive-ones-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left=0,right,n=nums.length;
+        for(right=0;right<n;right++){
+            if(nums[right]==0)
+            k--;
+            if(k<0){
+                if(nums[left]==0){
+                    k++;
+                }
+                left++;
+            }
+        }
+        return right-left;
+    }
+}
